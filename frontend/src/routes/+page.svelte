fe5f1efd1d24aa949d88ddef5c3d83db25fb1994
szewczukk <dmitro.szewczuk@gmail.com>
2026-03-01 10:15:08 +0100

    add random images

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { products } from "$lib/constants/products";
+import { products } from "$lib/constants/products";
 </script>
 
 <ul class="flex gap-8">
   {#each products as product}
     <li>
         <a href={`/products/${product.id}`}>
-            <div class="w-[256px] h-[256px] bg-slate-300"></div>
+        <img src="https://picsum.photos/seed/${product.id}/256/256" alt={product.name} class="w-[256px] h-[256px]" />
             <div class="flex justify-between">
                 <p>{product.name}</p>
                 <p>{product.price}zł</p>
