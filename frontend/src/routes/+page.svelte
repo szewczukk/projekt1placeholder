c2d8d3b32651596fe94cb31bf0f4151b68537685
gbrzoskwinia <brzoskwinia.gabriel@gmail.com>
2026-03-01 10:15:04 +0100

    feat: product details page baseline

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
-const products = [
-	{
-		id: 1,
-		name: "Product 1",
-		price: 100,
-	},
-	{
-		id: 2,
-		name: "Product 2",
-		price: 150,
-	},
-	{
-		id: 2,
-		name: "Product 3",
-		price: 400,
-	},
-];
+	import { products } from "$lib/constants/products";
 </script>
 
 <ul class="flex gap-8">
