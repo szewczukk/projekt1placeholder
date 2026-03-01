b3ac8548f80e93bcc8e4499e1f1ef21050fb5171
szewczukk <dmitro.szewczuk@gmail.com>
2026-03-01 09:35:49 +0100

    add .vscode to gitignore

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+    },
+  ];
+</script>
+
+<div class="container mx-auto">
+  <ul class="flex gap-4">
+    {#each products as product}
+      <li>
+        <div class="w-[256px] h-[256px] bg-slate-300"></div>
+        <div>
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</div>
